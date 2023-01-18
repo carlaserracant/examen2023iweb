c82e099df8c4601dc5395d9ca92db3436f62a764
carlaserracant <80315096+carlaserracant@users.noreply.github.com>
2023-01-18 18:32:52 +0100

    new commit

---
+++ src/components/componenteVerUsuario.vue
@@ -0,0 +1,26 @@
+<script>
+
+export default{
+    data: function(){
+        return{
+            busqueda : "",
+            token: localStorage.getItem('Token'),
+            email: localStorage.getItem('NombreUsuario'),
+            usuario: localStorage.getItem('Usuario'),
+            imagen: localStorage.getItem('Imagen'),
+            conexion: localStorage.getItem('Conexion'),
+            caducidad: localStorage.getItem('Caducidad')
+        };
+    },
+    methods: {
+    getPlaceholder() {
+      return "Busque una vivienda segun su provincia";
+    },
+    getBusqueda(){
+        return " " + this.busqueda;
+    }
+    }
+  
+};
+
+</script>
