af71172c2d2b54f68201a3a607606890e440623d
carlaserracant <80315096+carlaserracant@users.noreply.github.com>
2023-01-18 18:24:45 +0100

    new commit

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,16 @@ import componenteVerReserva from '../components/componenteVerReserva.vue'
 import componenteVerAnuncio from '../components/componenteVerAnuncio.vue'
 import componenteListaReservas from '../components/componenteListaReservas.vue'
 import componenteTiempo from '../components/componenteTiempo.vue'
-
+import componenteVerUsuario from '../components/componenteVerUsuario.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    {
+      path: '/verUsuario',
+      name: 'verUsuario',
+      component: componenteVerUsuario
+    },
+    
     {
       path: '/',
       name: 'home',
