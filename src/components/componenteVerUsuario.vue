1e89ba5544d52fbf02bcc13010fc914f130acf13
carlaserracant <80315096+carlaserracant@users.noreply.github.com>
2023-01-18 18:44:53 +0100

    new commit

--- src/components/componenteVerUsuario.vue
+++ src/components/componenteVerUsuario.vue
@@ -18,5 +18,40 @@ export default{
 </script>
 
 <template>
-    <h1> {{ usuario }}</h1>
+    <table class="tabla-Grande" v-if="vivienda">
+
+        <td class="celda-Anuncio"><img :src='imagen' width="375" /></td>
+
+            <td class="celda-Anuncio">
+
+            <table class="tabla-Info">
+                <tr>
+                <td class="bold-text">Email:&nbsp;</td>
+                <td>{{ email }}</td>
+                </tr>
+
+                <tr>
+                <td class="bold-text">Nombre:&nbsp;</td>
+                <td>{{ usuario }}</td>
+                </tr>
+
+                <tr>
+                <td class="bold-text">Conexion:&nbsp;</td>
+                <td>{{ conexion }}</td>
+                </tr>
+
+                <tr>
+                <td class="bold-text">Caducidad:&nbsp;</td>
+                <td>{{ caducidad }}</td>
+                </tr>
+
+                <tr>
+                <td class="bold-text">Token:&nbsp;</td>
+                <td>{{ token }}</td>
+                </tr>
+
+            </table>
+
+        </td>
+    </table>
 </template>
