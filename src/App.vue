3f5c5291e9a8a27651fcd959793b0a53f5fd2f27
carlaserracant <80315096+carlaserracant@users.noreply.github.com>
2023-01-18 17:41:44 +0100

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,53 @@
 <script>
 import { RouterLink, RouterView } from 'vue-router'
-
-export default{
+export default {
   data() {
     return {
-      token : localStorage.getItem('Token')
+      token: localStorage.getItem('Token'),
+      currentRoute: '/'
     };
+  },
+  watch: {
+    $route(to) {
+      this.currentRoute = to.path
+    }
+  },
+  methods: {
+    //Borramos los datos guardados
+    cerrarSesion() {
+      localStorage.clear();
+      this.token = undefined;
+      location.reload();
+    }
   }
 }
-
 </script>
 
 
 <template>
-  
+
   <header>
 
     <div class="logo">
       <h2 class="logo-nombre">
-        <router-link to="/"> The Good House </router-link>
+        <router-link to="/"> Parking </router-link>
       </h2>
     </div>
 
     <nav>
       <router-link to="/"> Inicio </router-link>
-      <router-link to="/listaViviendas" > Ver Anuncios </router-link>
-      <router-link v-if="token" to="/crearAnuncio"> Crear Anuncios </router-link>
-      <router-link v-if="token" to="/listaReservas"> Ver Reservas </router-link>
+      <router-link href="" v-if="token" to="/verUsuario">Información Usuario</router-link>
+      <router-link v-if="currentRoute === '/' && token" to="/" @click="cerrarSesion()">Cerrar sesión</router-link>
     </nav>
 
-    <div class="logo">
-      <a class="g_id_signin" data-type="standard" data-shape="pill" data-size="large" ></a>
+    <div class="logo" v-if="!token">
+      <a class="g_id_signin" data-type="standard" data-shape="pill" data-size="large"></a>
     </div>
 
   </header>
+
   <div padding-top="20px">
-    <router-view/>
+    <router-view />
   </div>
 
 </template>
