aaef0e62ddf65cfb7a3ef78b801c491e977dd986
carlaserracant <80315096+carlaserracant@users.noreply.github.com>
2023-01-18 19:48:19 +0100

    new commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import componenteHome from '../components/componenteHome.vue'
-import componenteListaAnuncio from '../components/componenteListaAnuncios.vue'
-import saveComponent from '../components/saveComponent.vue'
-import componenteVerReserva from '../components/componenteVerReserva.vue'
-import componenteVerAnuncio from '../components/componenteVerAnuncio.vue'
-import componenteListaReservas from '../components/componenteListaReservas.vue'
-import componenteTiempo from '../components/componenteTiempo.vue'
 import componenteVerUsuario from '../components/componenteVerUsuario.vue'
 
 const router = createRouter({
